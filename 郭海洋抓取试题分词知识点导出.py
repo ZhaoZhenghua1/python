0bc55aaecc538c0707354f6962804277e9bdff96
zhaozhenghua <910084213@qq.com>
2018-11-29 10:59:14 +0800

    00

--- 郭海洋抓取试题分词知识点导出.py
+++ 郭海洋抓取试题分词知识点导出.py
@@ -12,23 +12,8 @@ import xlwt
 import xlsxwriter
 import requests
 
-def GetAllFiles(path):
-    ret = []
-    if os.path.isfile(path):
-        if '$' in path:
-            return ret
-        extension = os.path.splitext(path)[1]
-        if extension == '.docx' or extension == '.doc':
-            ret.append(path)
-    elif os.path.isdir(path):
-        list=os.listdir(path)
-        for i in range(0, len(list)):
-            ret += (GetAllFiles(path + '\\' + list[i]))
-    return ret
-
-filePath = r"D:\试题资料\历史试卷\历史学科已收集原试卷：初中（4479）\初中历史（4479）\中考、会考226"#sys.argv[1]r"D:\ttt"#
+filePath = sys.argv[1]
 print("文件路径：" + filePath)
-allfiles = GetAllFiles(filePath)
 
 def ParseExcel(file):
     wb = open_workbook(file)
@@ -57,21 +42,13 @@ def ParseExcel(file):
         lst.append(values)
     return lst
 
-knowledgeFile = r'D:\试题资料\历史识别中易出错词汇.xls'#sys.argv[2]#'历史识别中易出错词汇.xls'#sys.argv[2]'政治识别中易出错词汇（含知识点标准形式、非标准形式、单特征词、歧义型知识点）.xlsx'#sys.argv[2]#
+knowledgeFile = sys.argv[2]#
 print("知识点词汇路径：" + knowledgeFile)
 knowledgeValues = ParseExcel(knowledgeFile)
 
-threadLock = threading.Lock()
-def GetAvailableFile():
-    threadLock.acquire()
-    if len(allfiles) != 0:
-        temp = allfiles.pop()
-    else:
-        temp = '';
-    threadLock.release()
-    return temp
-
+#等价关系路径
 outputPathEquivalence = os.path.dirname(knowledgeFile) + '\\' + os.path.splitext(os.path.basename(knowledgeFile))[0] + 'Equivalence'
+#包含关系路径
 outputPathInclusion = os.path.dirname(knowledgeFile) + '\\' + os.path.splitext(os.path.basename(knowledgeFile))[0] + 'Inclusion'
 import shutil
 print('output Equivalence:' + outputPathEquivalence)
@@ -92,103 +69,6 @@ def error(str):
     document.write((str + '\r\n').encode('utf-16'))
     threadLockError.release()
 
-def GetWordText(file):
-    dll = windll.LoadLibrary('CCSInteroperate.dll')
-    jsn = c_wchar_p(dll.FileStandardise(file))
-    result = json.loads(jsn.value)
-    windll.LoadLibrary('OleAut32.dll').SysFreeString(jsn)
-    data = result['Data']
-    fileSt = json.loads(data)
-    if fileSt['Stauts'] != 1:
-        error("File:" + file + " Out:" + str(fileSt));
-        return ''
-    else:
-        return fileSt['Data']['Text']
-
-
-dll = windll.LoadLibrary('AllSubjectsKnowledgeRec.dll')
-dll.InitService('172.16.63.20', '9102')
-
-def PreProcessing(subject, text):
-    dll = windll.LoadLibrary('AllSubjectsKnowledgeRec.dll')
-    jsn = c_wchar_p(dll.PreProcessingInterface(subject, text))
-    result = json.loads(jsn.value)
-    windll.LoadLibrary('OleAut32.dll').SysFreeString(jsn)
-    return result['Data']
-
-def SplitString(string, regex):
-    ret = re.split(regex, string)
-    ret = [x for x in ret if len(x.strip()) != 0]#去除空句子
-    ret = [re.sub(r"\s+", "", sentence, flags=re.UNICODE) for sentence in ret]#去除句子中的空白字符
-    return ret
-
-def SplitSentences(text):
-    ret = []
-    lines = text.splitlines(False)
-    for line in lines:
-        segs = SplitString(line, r'。|？|！|，|,')
-        ret += segs
-    return ret
-
-AllSegments = set()
-threadLockSeg = threading.Lock()
-def AddToBuffer(seg):
-    threadLockSeg.acquire()
-    AllSegments.add(seg)
-    #print(len(AllSegments))
-    threadLockSeg.release()
-
-class TestThread(threading.Thread):
-     def __init__(self):
-        threading.Thread.__init__(self)
-
-     def recognise(self):
-        while True:
-            path = GetAvailableFile()
-            print("dealing==:" + path)
-            if len(path) == 0:
-                break
-            #try:
-            if True:
-                text = GetWordText(path)
-                text = PreProcessing('H', text)
-                if len(text) == 0:
-                    continue
-                sentences =  SplitSentences(text)
-                for sent in sentences:
-                    global AllSegments
-                    AddToBuffer(sent)
-            #except Exception as e:
-            #    error(traceback.format_exc())
-     def run(self):
-        self.recognise()
-
-print('------------------------------------GetAllText--------------------------')
-threads=[TestThread(),TestThread(),TestThread(),TestThread(),TestThread()]
-for t in threads:
-    t.start()
-
-for t in threads:
-    t.join()
-print('------------------------------------FINISH GetAllText--------------------')
-sortedSentences = list(AllSegments)
-def SortByLen(array):
-    return len(str(array))
-sortedSentences.sort(key=SortByLen)
-
-def Segment(sentences):
-    sendData = json.loads(r'{"SubjectId": "","SentenceList": [],"FormulaList": [],"ImageList": [],"GetConfigInfoURL": "172.16.63.20:9102"}')
-    sendData['SubjectId'] = subject
-    sendData['SentenceList'] = sentences
-    url = r'http://172.16.63.25:10104/api/KRWebApi/ChWordSegmentation'
-    header = {'accept': 'application/json', 'Content-Type': 'application/json'}
-    ret = requests.post(url= url, data = json.dumps(sendData).encode('utf-8'), headers = header)
-    segresult = json.loads(ret.text)
-    if segresult['Status'] != 0:
-        error(segresult)
-        exit(0)
-    return segresult['Data']
-
 def StandardFileName(file):
     file = file.replace('\\','＼')
     file = file.replace('/','／')
@@ -201,9 +81,10 @@ def StandardFileName(file):
     file = file.replace('|','｜')
     return file
 
-def GetRightPath(sheetName, knowledge):
-    fileName = StandardFileName(knowledge)
-    path = outputPathEquivalence + '\\' + sheetName
+pathlock = threading.Lock()
+def doPath(path, sheetName, fileName):
+    pathlock.acquire()
+    path = path + '\\' + sheetName
     if not os.path.exists(path):
         os.makedirs(path)
     while os.path.exists(path + '\\' + fileName):
@@ -211,9 +92,16 @@ def GetRightPath(sheetName, knowledge):
     path += '\\' + fileName
     if not os.path.exists(path):
         os.makedirs(path)
+    pathlock.release()
     return path
+def GetERightPath(sheetName, knowledge):
+    fileName = StandardFileName(knowledge)
+    return doPath(outputPathEquivalence, sheetName, fileName)
+def GetIRightPath(sheetName, knowledge):
+    fileName = StandardFileName(knowledge)
+    return doPath(outputPathInclusion, sheetName, fileName)
 
-def CreateFile(filename):
+def CreateFile(filename, type):
     workbook = xlsxwriter.Workbook(filename)
     knowSheet = workbook.add_worksheet('知识点语境')
     red = workbook.add_format({'color': 'red'})
@@ -223,7 +111,11 @@ def CreateFile(filename):
     knowSheet.set_column('A:A', 90)
     #knowSheet.set_column('B:B', 60)
     knowSheet.write('A1', '知识点语境',bold)
-    knowSheet.write('B1', '知识点',bold)
+    if type == 0:
+        knowSheet.write('B1', '知识点',bold)
+    elif type == 1:
+        knowSheet.write('B1', '知识点分词',bold)
+        knowSheet.write('C1', '知识点',bold)
     knowSheet.freeze_panes(1, 0)
     
     return workbook,knowSheet,red,text_wrap
@@ -237,42 +129,147 @@ def GetSubject(path):
 subject = GetSubject(knowledgeFile)
 print('subject:' + subject)
 
-sentenceAndSegments = Segment(sortedSentences)
+def GetReadData():
+    arr = []
+    read = open(filePath + r'\allfiledata.txt', 'r',encoding = 'utf-16')
+    line = read.readline()
+    while line:
+        if line.startswith(u'\ufeff'):
+            line = line.encode('utf8')[3:].decode('utf8')
+        arr.append(json.loads(line))
+        line = read.readline()
+    read.close()
+    return arr
+sentenceAndSegments = GetReadData()
 
 isLiKe = subject in ['B','D','E','F']
 
-def Indexes(sentence, seg):
-    ret = []
-    index = sentence.find(seg)
-    while index != -1:
-        ret.append(index)
-        index = sentence.find(seg, index + len(seg))
-    return ret
+def GetAvailableKnowledge():
+    for values in knowledgeValues:
+        sheetName = values['sheetName']
+        header = values['header']
+        sheetValues = values['sheetValues']
+        for values in sheetValues:
+            for knowledge in values:
+                yield sheetName, knowledge
 
-for values in knowledgeValues:
-    sheetName = values['sheetName']
-    header = values['header']
-    sheetValues = values['sheetValues']
-    for values in sheetValues:
-        for knowledge in values:
+def LongString(length, string):
+    string = '0'*(length - len(string)) + string
+    return string
+class SortByKnowledge:
+    def __init__(self, knowledge):
+        self.knowledge = knowledge
+    def __call__(self, array):
+        knowsegs = []
+        for seg in array:
+            if seg.find(self.knowledge) != -1 and self.knowledge != seg:
+                if seg not in knowsegs:
+                    knowsegs.append(seg)
+        return LongString(3,str(len(knowsegs))) + LongString(50, knowsegs[0]) + LongString(3, str(len(str(array)))) + LongString(500, str(array))
+
+def DoInclude(IncludeArraySegs, sheetName, knowledge):
+    path = GetIRightPath(sheetName, knowledge)
+    IncludeArraySegs.sort(key=SortByKnowledge(knowledge))
+    allCount = 0
+    row = 2
+    currentSeg = ''
+    filename = StandardFileName(knowledge)
+    workbook,knowSheet,red,text_wrap = CreateFile(path + '\\' + filename + '.xls', 1)
+    for segs in IncludeArraySegs:
+        string_parts = []
+        knowSegs = []
+        for seg in segs:
+            index = seg.find(knowledge) 
+            if index == -1:
+                string_parts.append(seg)
+            else:
+                if seg not in knowSegs:
+                    knowSegs.append(seg)
+                start = seg[0:index]
+                if len(start) != 0:
+                    string_parts.append(start)
+                if knowSegs[0] != currentSeg:
+                    currentSeg = knowSegs[0]
+                    if row > 1001:
+                        workbook.close()        
+                        os.rename(path + '\\' + knowledge + '.xls', path + '\\' + knowledge + ' ' + str(allCount) + '.xls')
+                        workbook,knowSheet,red,text_wrap = CreateFile(path + '\\' + knowledge + '.xls', 1)
+                        row = 2
+                string_parts.append(red)
+                string_parts.append(knowledge)
+                end = seg[index + len(knowledge) : len(seg)]
+                if len(end) != 0:
+                    string_parts.append(end)
+            string_parts.append('\\')
+        string_parts = string_parts[:-1]
+        if len(string_parts) == 2:
+            string_parts += ' '
+        string_parts.append(text_wrap)
+        knowSheet.write_rich_string('A' + str(row),*string_parts)
+        knowSheet.write('B' + str(row), "|".join(knowSegs))
+        knowSheet.write('C' + str(row), knowledge)
+        row += 1
+        allCount += 1
+    if row > 2:
+        workbook.close()        
+        os.rename(path + '\\' + filename + '.xls', path + '\\' + filename + ' ' + str(allCount) + '.xls')
+    temp = ((' ' + str(allCount)) if allCount > 0 else ' Empty')
+    while os.path.exists(path + temp):
+        temp += ' -'
+    os.rename(path, path + temp)
+
+availableKnowIte = GetAvailableKnowledge()
+threadLockSeg = threading.Lock()
+def GetKnow():
+    threadLockSeg.acquire()
+    return next(availableKnowIte)
+    threadLockSeg.release()
+
+class DThread(threading.Thread):
+     def __init__(self):
+        threading.Thread.__init__(self)
+        self.lock = threading.Lock()
+
+     def run(self):
+        while True:
+            self.lock.acquire()
+            try:
+                sheetName, knowledge = next(availableKnowIte)
+            except StopIteration:
+                break
+            self.lock.release()
             if knowledge and knowledge.strip():
-                path = GetRightPath(sheetName, knowledge)
+                filename = StandardFileName(knowledge)
+                print("Doing--:" + knowledge)
+                path = GetERightPath(sheetName, knowledge)
+                IncludeArray = []
+                allCount = 0
                 row = 2
-                workbook,knowSheet,red,text_wrap = CreateFile(path + '\\' + 'output.xls') 
+                workbook,knowSheet,red,text_wrap = CreateFile(path + '\\' + knowledge + '.xls', 0) 
                 for sentAndSeg in sentenceAndSegments:
-                    SentenceContent = sentAndSeg['SentenceContent']
+                    if row > 1001:
+                        workbook.close()        
+                        os.rename(path + '\\' + filename + '.xls', path + '\\' + filename + ' ' + str(allCount) + '.xls')
+                        workbook,knowSheet,red,text_wrap = CreateFile(path + '\\' + filename + '.xls', 1)
+                        row = 2
+                    SentenceContent = sentAndSeg['s']
                     if len(SentenceContent.strip()) == 0:
                         continue
-                    SentenceSegWords = sentAndSeg['SentenceSegWords']
+                    SentenceSegWords = sentAndSeg['g']
                     string_parts = []
                     if isLiKe:#添加理科处理逻辑
-                        if knowledge not in SentenceSegWords:#理科
+                        #包含关系
+                        if str(SentenceSegWords).find(knowledge)!= -1 and knowledge not in SentenceSegWords:
+                            IncludeArray.append(SentenceSegWords)
+                        #处理等价关系
+                        if knowledge in SentenceSegWords:#理科
+                            for seg in SentenceSegWords:
+                                if seg == knowledge:
+                                    string_parts.append(red)
+                                string_parts.append(seg)
+                                string_parts.append('\\')
+                        else:
                             continue
-                        for seg in SentenceSegWords:
-                            if seg == knowledge:
-                                string_parts.append(red)
-                            string_parts.append(seg)
-                            string_parts.append('\\')
                     else:#文科              
                         index = SentenceContent.find(knowledge)        
                         if index == -1:
@@ -301,8 +298,24 @@ for values in knowledgeValues:
                     knowSheet.write_rich_string('A' + str(row),*string_parts)
                     knowSheet.write('B' + str(row), knowledge)
                     row += 1
-                workbook.close()        
-                os.rename(path + '\\' + 'output.xls', path + '\\' + 'output' + str(row) + '.xls')
+                    allCount += 1
+                if row > 2:
+                    workbook.close()        
+                    os.rename(path + '\\' + filename + '.xls', path + '\\' + filename + ' ' + str(allCount) + '.xls')
+                temp = ((' ' + str(allCount)) if allCount > 0 else ' Empty')
+                while os.path.exists(path + temp):
+                    temp += ' -'
+                os.rename(path, path + temp)
+                DoInclude(IncludeArray, sheetName, knowledge)
 
-    
 
+print('------------------------------------ParseKnowledge--------------------------')
+threadNum = 10
+threads = []
+for i in range(0, threadNum):
+    threads.append(DThread())
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
+print('------------------------------------FINISH ParseKnowledge--------------------')
